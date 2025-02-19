d51a42dc230ad06022b0409af1ec2f2595f12935
Slavencia <clavakopylov2010@gmail.com>
2025-02-19 16:47:57 +0300

    Изменил

--- bot.py
+++ bot.py
@@ -11,14 +11,14 @@ def start(message):
 def ban_user(message):
     if message.reply_to_message:
         chat_id = message.chat.id
-         # сохранение id и статуса пользователя, отправившего сообщение
+         # Это я изменил
         user_id = message.reply_to_message.from_user.id
         user_status = bot.get_chat_member(chat_id, user_id).status 
-         # проверка пользователя
+         
         if user_status == 'administrator' or user_status == 'creator':
             bot.reply_to(message, "Невозможно забанить администратора.")
         else:
-            bot.ban_chat_member(chat_id, user_id) # пользователь с user_id будет забанен в чате с chat_id
+            bot.ban_chat_member(chat_id, user_id) # 
             bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
