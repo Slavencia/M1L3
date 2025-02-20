41ca68cf490677c5dbfea79812fd19c4912495d3
Slavencia <clavakopylov2010@gmail.com>
2025-02-20 18:23:48 +0300

    добавид новую функцию для выдачи администратора

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import telebot # библиотека telebot
+import time
 from config import token # импорт токена
 
 bot = telebot.TeleBot(token) 
@@ -22,5 +23,29 @@ def ban_user(message):
             bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был забанен.")
     else:
         bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
-#Изменения
+
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'Я принял нового пользователя!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
+
+@bot.message_handler(commands=['admin'])
+def admin_user(message):
+    if message.reply_to_message:
+        chat_id = message.chat.id
+        user_id = message.reply_to_message.from_user.id
+        user_status = bot.get_chat_member(chat_id, user_id).status 
+        #print("Тут все норм")
+        if user_status == 'administrator' or user_status == 'creator':
+            bot.reply_to(message, "Невозможно дать администратора администратору.")
+            #print("Тут да")
+        else:
+            #bot.get_chat_administrators(chat_id, user_id) 
+            
+            bot.reply_to(message, f"Пользователь @{message.reply_to_message.from_user.username} был назначен админом.")
+            bot.get_chat_administrators(chat_id)
+            
+    else:
+            bot.reply_to(message, "Эта команда должна быть использована в ответ на сообщение пользователя, которого вы хотите забанить.")
+            #print("Тут тоже работает")
 bot.infinity_polling(none_stop=True)
